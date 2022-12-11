4b73b38983591df72b54b4fe16203cfb8f5419e6
CrisPaguntalan <crisestephanpaguntalan@gmail.com>
2022-12-12 05:45:24 +0800

    added navigation

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const AboutPage = document.getElementById("AboutPage");
+const ProjectsPage = document.getElementById("ProjectsPage");
+const ContactPage = document.getElementById("ContactPage");
+const HomePage = document.getElementById("HomePage");
+
+function scrollToAbout() {
+    AboutPage.scrollIntoView(true);
+}
+
+function scrollToProjects() {
+    ProjectsPage.scrollIntoView(true);
+}
+
+function scrollToContact() {
+    ContactPage.scrollIntoView(true);
+}
+
+function scrollToHome() {
+    HomePage.scrollIntoView(true);
+}
